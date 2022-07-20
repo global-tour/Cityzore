3dce6c565009a6176dd4a78c4161815b02d884a3
Mesut Aydın <xmesut123@gmail.com>
2022-07-20 17:12:34 +0300

    Search Design

--- resources/views/frontend-partials/header.blade.php
+++ resources/views/frontend-partials/header.blade.php
@@ -2,6 +2,7 @@
 $commonFunctions = new \App\Http\Controllers\Helpers\CommonFunctions;
 $langCode = !is_null(session()->get('userLanguage')) ? session()->get('userLanguage') : 'en';
 $langCodeForUrl = $langCode == 'en' ? '' : $langCode;
+$langs = \App\Language::where('isActive', 1)->get()->pluck('code')->toArray();
 ?>
 
 
@@ -20,10 +21,10 @@ $langCodeForUrl = $langCode == 'en' ? '' : $langCode;
 
                             </li>
                             @if(strpos(url()->current(), $commonFunctions->getRouteLocalization('credit-card-details-for-shared-cart')) == false)
-                            <li><input type="hidden" id="sessionCurrency" value="{{session()->get('currencyCode')}}"></li>
-                            <li class="currencyLi">
+                                <li><input type="hidden" id="sessionCurrency" value="{{session()->get('currencyCode')}}"></li>
+                                <li class="currencyLi">
 
-                            </li>
+                                </li>
                             @endif
                         </ul>
                     </div>
@@ -40,52 +41,52 @@ $langCodeForUrl = $langCode == 'en' ? '' : $langCode;
                                         <a href="{{url($langCodeForUrl.'/'.(new App\Http\Controllers\Helpers\CommonFunctions)->getRouteLocalization('check-booking'))}}" style="color: #337ab7;">{{__('checkYourBooking')}}</a>
                                     </li>
                                     <li><form id="loginModalForm" method="POST" action="{{ url($langCodeForUrl.'/login') }}">
-                                        @csrf
-                                        <input hidden type="radio" checked name="guard" id="guard" value="web" >
-                                        <div id="myModal" class="modal">
-                                            <div class="modal-content">
-                                                <span class="close">&times;</span>
-                                                <section>
-                                                    <div class="tr-regi-form">
-                                                        <h4>{{__('signin')}}</h4>
-                                                        <div style="display: none;" id="loginModalFormAlertDiv" class="alert alert-danger" role="alert">
+                                            @csrf
+                                            <input hidden type="radio" checked name="guard" id="guard" value="web" >
+                                            <div id="myModal" class="modal">
+                                                <div class="modal-content">
+                                                    <span class="close">&times;</span>
+                                                    <section>
+                                                        <div class="tr-regi-form">
+                                                            <h4>{{__('signin')}}</h4>
+                                                            <div style="display: none;" id="loginModalFormAlertDiv" class="alert alert-danger" role="alert">
 
-                                                        </div>
-                                                        <div class="row">
-                                                            <div class="input-field col s12">
-                                                                <input style="height: 5rem!important; border-radius: 0!important;" id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="{{__('email')}}">
-                                                                <label>{{__('email')}}</label>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="input-field col s12">
+                                                                    <input style="height: 5rem!important; border-radius: 0!important;" id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="{{__('email')}}">
+                                                                    <label>{{__('email')}}</label>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="input-field col s12">
+                                                                    <input style="height: 5rem!important; border-radius: 0!important;" id="password" type="password" class="form-control" name="password" required autocomplete="current-password" placeholder="{{__('password')}}">
+                                                                    <label>{{__('password')}}</label>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="input-field col s12">
+                                                                    <button id="loginSubmitButton" type="submit" class="btn btn-primary btn-block">
+                                                                        {{__('login')}}
+                                                                    </button>
+                                                                </div>
+                                                            </div>
+                                                            <a style="box-shadow: none;font-size: 11px;margin-top: 15px;" class="btn btn-link" href="{{ url($langCodeForUrl.'/password/reset') }}">
+                                                                {{__('forgotYourPassword')}}
+                                                            </a>
+                                                            <br><a style="box-shadow: none;font-size: 11px;" href="{{url($langCodeForUrl.'/register')}}" class="btn btn-link">{{__('register')}}</a>
+                                                            <div class="soc-login">
+                                                                <ul>
+                                                                    <li></li>
+                                                                    <li><a style="background: #DB4437!important;" href="{{url($langCodeForUrl.'/login/google')}}"><i class="fa fa-google-plus gp1"></i> Google</a> </li>
+                                                                    <li></li>
+                                                                </ul>
                                                             </div>
                                                         </div>
-                                                        <div class="row">
-                                                            <div class="input-field col s12">
-                                                                <input style="height: 5rem!important; border-radius: 0!important;" id="password" type="password" class="form-control" name="password" required autocomplete="current-password" placeholder="{{__('password')}}">
-                                                                <label>{{__('password')}}</label>
-                                                            </div>
-                                                        </div>
-                                                        <div class="row">
-                                                            <div class="input-field col s12">
-                                                                <button id="loginSubmitButton" type="submit" class="btn btn-primary btn-block">
-                                                                    {{__('login')}}
-                                                                </button>
-                                                            </div>
-                                                        </div>
-                                                        <a style="box-shadow: none;font-size: 11px;margin-top: 15px;" class="btn btn-link" href="{{ url($langCodeForUrl.'/password/reset') }}">
-                                                            {{__('forgotYourPassword')}}
-                                                        </a>
-                                                        <br><a style="box-shadow: none;font-size: 11px;" href="{{url($langCodeForUrl.'/register')}}" class="btn btn-link">{{__('register')}}</a>
-                                                        <div class="soc-login">
-                                                            <ul>
-                                                                <li></li>
-                                                                <li><a style="background: #DB4437!important;" href="{{url($langCodeForUrl.'/login/google')}}"><i class="fa fa-google-plus gp1"></i> Google</a> </li>
-                                                                <li></li>
-                                                            </ul>
-                                                        </div>
-                                                    </div>
-                                                </section>
+                                                    </section>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </form></li>
+                                        </form></li>
                                 @else
                                     <li>
                                         <a class="waves-effect dropdown-button top-user-pro" href="#" data-activates="top-menu" style="height: 28px;margin-top: 1px;line-height: 17px;" id="myBtn">{{__('welcome')}}, {{auth()->user()->name}}<i class="fa fa-angle-down" aria-hidden="true"></i></a>
@@ -124,24 +125,34 @@ $langCodeForUrl = $langCode == 'en' ? '' : $langCode;
     </div>
     <div class="top-logo" data-spy="affix" data-offset-top="250">
         <div class="container">
-            <div class="row">
-                <div class="col-md-12">
+            <div class="row d-flex align-items-center">
+                <div class="col-md-2">
                     <div class="wed-logo">
                         <a id="parisCityTours" href="{{url('/')}}"><img src="{{asset('img/paris-city-tours-logo.webp')}}" alt="Paris City Tours" />
                         </a>
                     </div>
-                        <form method="GET" class="tourz-search-form tour-form-one hidden-xs hidden-sm" action="{{url($langCodeForUrl.'/s')}}" autocomplete="off" style="width: 60%;">
-                            <div class="input-field" style="margin-top: 0!important;padding-top:4px!important;">
-                                <input type="text" id="select-search" name="q" class="searchInput" style="" autocomplete="off">
-                                <div class="suggestions-container" style="">
-
+                </div>
+                <div class="col-md-5">
+                    @if(!request()->is('/', $langs))
+                        <form method="GET" class="tourz-search-form tour-form-two" action="{{url($langCodeForUrl.'/s')}}"
+                              autocomplete="off">
+                            <div class="home-banner-search">
+                                <div class="search-bar header-search-area">
+                                    <input type="text" placeholder="{{__('searchLocationsAttractions')}}" name="q" class="search-field light-border" id="searchInput">
+                                    <button type="submit" class="search-button">SEARCH</button>
+                                    <div class="suggestions-container"></div>
                                 </div>
-                                <label for="select-search" class="search-hotel-type">{{__('searchLocationsAttractions')}}</label>
-                            </div>
-                            <div class="input-field" style="margin-top: 0!important;padding-top:4px!important;width: 10%;">
-                                <input type="submit" value="search" class="waves-effect waves-light tourz-sear-btn">
+                                <div class="mobile-search-bar">
+                                    <a href="javascript:;" class="mobile-search-field" id="mobile-search-button">
+                                        <i class="glyphicon glyphicon-search"></i>
+                                        Search
+                                    </a>
+                                </div>
                             </div>
                         </form>
+                    @endif
+                </div>
+                <div class="col-md-5">
                     <div class="main-menu">
                         <ul>
                             <li class="hidden-xs"><a href="{{url('/')}}">{{__('home')}}</a>
